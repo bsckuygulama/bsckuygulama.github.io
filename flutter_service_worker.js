e68579452e64079911790bd9ccb2fc87a54a2bc5
bsckuygulama <80956994+bsckuygulama@users.noreply.github.com>
2021-03-20 21:31:32 +0300

    20032021

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,9 +15,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "74b245d5d27a49dd3b5fb599ed66a747",
-"/": "74b245d5d27a49dd3b5fb599ed66a747",
-"main.dart.js": "7c50518f4e1fe3253861b8b8410450c2",
+"index.html": "211d320f28fec9063261861dff3d9fe3",
+"/": "211d320f28fec9063261861dff3d9fe3",
+"main.dart.js": "4c5ce4c63c2c614124acbd97cfef2751",
 "manifest.json": "6f690f29f02f5fd3720a4298f2436b38",
 "version.json": "70f5274276b86033099122f14ccbe343"
 };
